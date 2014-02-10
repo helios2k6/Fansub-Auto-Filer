a463b39c8e39c62961a66a8d3f0dc344294b0d18
Andrew Johnson <helios2k6@gmail.com>
2014-02-10 03:02:36 +0000

    added an option to specify copy or move operations. this is mandatory

--- Media Library Auto Filer/Program.cs
+++ Media Library Auto Filer/Program.cs
@@ -18,6 +18,11 @@ namespace Media_Library_Auto_Filer
 {
 	public static class Program
 	{
+		/// <summary>
+		/// Specifies whether to move or copy a file
+		/// </summary>
+		public enum FileAction { MOVE, COPY }
+
 		private static Stopwatch Timer;
 		private static ConcurrentDictionary<string, long> StartTimes;
 		private static ConcurrentDictionary<string, long> EndTimes;
@@ -74,34 +79,91 @@ namespace Media_Library_Auto_Filer
 			var builder = new StringBuilder();
 			builder.Append("Media File Auto Filer").AppendLine();
 			builder.Append("Description: Automatically moves anime files in the current directory to any of the detected anime folders");
-			builder.Append("Usage: <this program> <directory to scan> [folders...]").AppendLine();
+			builder.Append("Usage: <this program> <directory to scan> <copy or move files> <repository folder> [additional repository folders...]").AppendLine();
 
 			Console.Write(builder.ToString());
 		}
 
-		private static void SafeMoveFile(string sourceFile, string destFile)
-		{
+		private static void SafeAction(Action fileAction, Action<Exception> errorReaction)
+		{ 
 #if PROFILE_ONLY
 			return;
 #endif
 			try
 			{
-				File.Move(sourceFile, destFile);
-				Console.WriteLine(string.Format("[INFO]: Successfully moved file {0} to {1}", sourceFile, destFile));
-			}
-			catch (Exception e)
+				fileAction.Invoke();
+			}catch(Exception e)
 			{
-				Console.WriteLine(string.Format(
-					"[ERROR]: Could not move file {0} to {1}. Reason: {2}",
-					sourceFile,
-					destFile,
-					e.Message));
+				errorReaction.Invoke(e);
 			}
+		}
+
+		private static void MoveFile(string source, string dest)
+		{
+			File.Move(source, dest);
+			Console.WriteLine(string.Format("[INFO]: Successfully moved file {0} to {1}", source, dest));
+		}
+
+		private static void MoveReaction(string source, string dest, Exception e)
+		{
+			Console.WriteLine(string.Format(
+				"[ERROR]: Could not move file {0} to {1}. Reason: {2}",
+				source,
+				dest,
+				e.Message));
+		}
+
+		private static void CopyFile(string source, string dest)
+		{
+			File.Copy(source, dest);
+			Console.WriteLine(string.Format("[INFO]: Successfully copied file {0} to {1}", source, dest));
+		}
+
+		private static void CopyReaction(string source, string dest, Exception e)
+		{
+			Console.WriteLine(string.Format(
+				"[ERROR]: Could not copy file {0} to {1}. Reason: {2}",
+				source,
+				dest,
+				e.Message));
+		}
+
+		private static IEnumerable<DriveHolding> GetDrives(string[] arguments)
+		{
+			for (var i = 2; i < arguments.Length; i++)
+			{
+				if (Directory.Exists(arguments[i]))
+				{
+					yield return (new DriveHolding(arguments[i]));
+				}
+			}
+		}
+
+		private static FileAction? GetFileAction(string[] arguments)
+		{
+			if (arguments == null || arguments.Length < 2)
+			{
+				return null;
+			}
+
+			string secondArgument = arguments[1];
+
+			if (secondArgument.Equals("COPY", StringComparison.OrdinalIgnoreCase))
+			{
+				return FileAction.COPY;
+			}
+
+			if (secondArgument.Equals("MOVE", StringComparison.OrdinalIgnoreCase))
+			{
+				return FileAction.MOVE;
+			}
+
+			return null;
 		}
 
 		public static void Main(string[] args)
 		{
-			if (args.Length < 2)
+			if (args.Length < 3)
 			{
 				PrintHelp();
 				return;
@@ -115,15 +177,14 @@ namespace Media_Library_Auto_Filer
 				return;
 			}
 
-			var allDrives = new List<DriveHolding>();
-			for (var i = 1; i < args.Length; i++)
+			var fileAction = GetFileAction(args);
+			if(fileAction == null)
 			{
-				if (Directory.Exists(args[i]))
-				{
-					allDrives.Add(new DriveHolding(args[i]));
-				}
+				PrintHelp();
+				return;
 			}
 
+			var allDrives = GetDrives(args);
 			if (!allDrives.Any())
 			{
 				PrintHelp();
@@ -143,11 +204,11 @@ namespace Media_Library_Auto_Filer
 				.Union(Directory.EnumerateFiles(directoryToScan, "*.wmv"));
 
 			InitializeProfilingEnvironment();
-			ProcessAllMediaFiles(allMediaFiles, manager);
+			ProcessAllMediaFiles(allMediaFiles, manager, fileAction.Value);
 			PrintStatistics();
 		}
 
-		private static void ProcessAllMediaFiles(IEnumerable<string> allMediaFiles, FilingManager.FilingManager manager)
+		private static void ProcessAllMediaFiles(IEnumerable<string> allMediaFiles, FilingManager.FilingManager manager, FileAction fileAction)
 		{
 			var resultDictionary = new ConcurrentDictionary<string, string>();
 			var failureBag = new ConcurrentBag<string>();
@@ -176,11 +237,30 @@ namespace Media_Library_Auto_Filer
 			foreach (var resultEntry in resultDictionary)
 			{
 				var originalFile = resultEntry.Key;
+
 				var destFolder = resultEntry.Value;
 				var justFileName = Path.GetFileName(originalFile);
 				var finalDest = Path.Combine(destFolder, justFileName);
 
-				SafeMoveFile(originalFile, finalDest);
+				//SafeMoveFile(originalFile, finalDest);
+
+				Action actionOnFile = default(Action);
+				Action<Exception> reactionOnFile = default(Action<Exception>);
+				switch(fileAction)
+				{
+					case FileAction.MOVE:
+						actionOnFile = () => MoveFile(originalFile, finalDest);
+						reactionOnFile = e => MoveReaction(originalFile, finalDest, e);
+						break;
+					case FileAction.COPY:
+						actionOnFile = () => CopyFile(originalFile, finalDest);
+						reactionOnFile = e => CopyReaction(originalFile, finalDest, e);
+						break;
+					default:
+						throw new InvalidOperationException("Unable to determine FileAction");
+				}
+
+				SafeAction(actionOnFile, reactionOnFile);
 			}
 
 			var builder = new StringBuilder();
